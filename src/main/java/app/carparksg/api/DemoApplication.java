50b69261f4b7d6abb43bd7efeb9701305edf552c
Reuben <reubentjl@gmail.com>
2019-08-21 12:29:00 +0000

    Update boilerplate controllers

--- src/main/java/app/carparksg/api/DemoApplication.java
+++ src/main/java/app/carparksg/api/DemoApplication.java
@@ -2,8 +2,10 @@ package app.carparksg.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"app.carparksg.api.controller", "app.carparksg.api.model", "app.carparksg.api.logic"})
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {

843f6e16e519d355cee5f00fff508a899ce225c4
Reuben <reubentjl@gmail.com>
2019-08-22 03:03:44 +0000

    Add equals method for ErrorResponse

--- src/main/java/app/carparksg/api/ErrorResponse.java
+++ src/main/java/app/carparksg/api/ErrorResponse.java
@@ -10,4 +10,18 @@ public class ErrorResponse {
     public String getError() {
         return this.error;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (object == null || object.getClass() != this.getClass()) {
+            return false;
+        }
+
+        ErrorResponse errorResponse = (ErrorResponse) object;
+        return error.equals(errorResponse.getError());
+    }
 }
